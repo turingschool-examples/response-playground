122cb45df2333256060d617d2e7a3ea3fa565d3f
Robbie Jaeger <jaeger.rob@gmail.com>
2020-05-13 09:52:34 -0600

    Add cors and body parser

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 app.set('port', process.env.PORT || 3001);
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (request, response) => {
   response.status(200).json({hello: 'world'});
